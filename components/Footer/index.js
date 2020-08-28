0bd8328745e8d850d04335d5c69db50eb1d4016e
GuilhermeBaccan <guilherme.sbaccan@gmail.com>
2020-08-28 11:41:42 -0300

    adding target.

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
         <div className='bg-gray-700 text-center font-bold text-white'>
             <div className='container mx-auto'>
                 Projeto desenvolvido por: {' '}
-                <a className='hover:text-gray-900' href='https://www.facebook.com/guilherme.baccan.7/'>Guilherme Baccan</a> / {' '}
-                <a className='hover:text-gray-900' href='https://www.linkedin.com/in/guilherme-baccan-5a29801b3/'>Linkedin</a> / {' '}
-                <a className='hover:text-gray-900' href='https://github.com/GuilhermeBaccan'>Github</a>
+                <a className='hover:text-gray-900' href='https://www.facebook.com/guilherme.baccan.7/' target='_blank'>Guilherme Baccan</a> / {' '}
+                <a className='hover:text-gray-900' href='https://www.linkedin.com/in/guilherme-baccan-5a29801b3/' target='_blank'>Linkedin</a> / {' '}
+                <a className='hover:text-gray-900' href='https://github.com/GuilhermeBaccan' target='_blank'>Github</a>
                 <div className='mt-2' >
                     <img className='inline p-4' src='/logo_semana_fsm.png' />
                     <img className='inline p-4' src='/logo_devpleno.png' />

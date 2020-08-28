698c33b3093ad3bd42bfb6ce868808030b35392e
GuilhermeBaccan <guilherme.sbaccan@gmail.com>
2020-08-28 11:36:04 -0300

    adding ponto.

--- pages/sobre.js
+++ pages/sobre.js
@@ -10,7 +10,7 @@ const Sobre = () => {
       <div>
         <p className='text-center my-20'>
           Projeto realizado durante a Semana FullStack Master, ministrada por Túlio Faria.<br />
-          Muito conhecimento adquirido em tão pouco tempo, com uma didática muito boa. Estou muito satisfeito<br />
+          Muito conhecimento adquirido em tão pouco tempo, com uma didática muito boa. Estou muito satisfeito.<br />
         </p>
       </div>
     </div>
